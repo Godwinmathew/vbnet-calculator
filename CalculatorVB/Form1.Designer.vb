8c54521ce99d9a5adc375a685e5f49f0b109e865
Godwin Mathew <christymathew2002@gmail.com>
2026-03-04 12:01:31 +0530

    Add files via upload

---
+++ CalculatorVB/Form1.Designer.vb
@@ -0,0 +1,273 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        txtDisplay = New TextBox()
+        Buttondiv = New Button()
+        Button_7 = New Button()
+        Buttonsub = New Button()
+        Buttonmulti = New Button()
+        Buttonadd = New Button()
+        Button_4 = New Button()
+        Button_9 = New Button()
+        Button_8 = New Button()
+        Button_1 = New Button()
+        Button_6 = New Button()
+        Button_5 = New Button()
+        ButtonEqual = New Button()
+        Button_3 = New Button()
+        Button_2 = New Button()
+        Button_0 = New Button()
+        Buttondot = New Button()
+        clear = New Button()
+        SuspendLayout()
+        ' 
+        ' txtDisplay
+        ' 
+        txtDisplay.Font = New Font("Microsoft Sans Serif", 8.25F)
+        txtDisplay.Location = New Point(73, 42)
+        txtDisplay.Name = "txtDisplay"
+        txtDisplay.Size = New Size(494, 23)
+        txtDisplay.TabIndex = 0
+        txtDisplay.Text = "0"
+        txtDisplay.TextAlign = HorizontalAlignment.Right
+        ' 
+        ' Buttondiv
+        ' 
+        Buttondiv.Font = New Font("Microsoft Sans Serif", 8.25F)
+        Buttondiv.Location = New Point(524, 128)
+        Buttondiv.Name = "Buttondiv"
+        Buttondiv.Size = New Size(43, 48)
+        Buttondiv.TabIndex = 16
+        Buttondiv.Text = "/"
+        Buttondiv.UseVisualStyleBackColor = True
+        ' 
+        ' Button_7
+        ' 
+        Button_7.Font = New Font("Microsoft Sans Serif", 8.25F)
+        Button_7.Location = New Point(83, 128)
+        Button_7.Name = "Button_7"
+        Button_7.Size = New Size(43, 48)
+        Button_7.TabIndex = 18
+        Button_7.Text = "7"
+        Button_7.UseVisualStyleBackColor = True
+        ' 
+        ' Buttonsub
+        ' 
+        Buttonsub.Font = New Font("Microsoft Sans Serif", 8.25F)
+        Buttonsub.Location = New Point(524, 263)
+        Buttonsub.Name = "Buttonsub"
+        Buttonsub.Size = New Size(43, 48)
+        Buttonsub.TabIndex = 19
+        Buttonsub.Text = "-"
+        Buttonsub.UseVisualStyleBackColor = True
+        ' 
+        ' Buttonmulti
+        ' 
+        Buttonmulti.Font = New Font("Microsoft Sans Serif", 8.25F)
+        Buttonmulti.Location = New Point(524, 195)
+        Buttonmulti.Name = "Buttonmulti"
+        Buttonmulti.Size = New Size(43, 48)
+        Buttonmulti.TabIndex = 20
+        Buttonmulti.Text = "*"
+        Buttonmulti.UseVisualStyleBackColor = True
+        ' 
+        ' Buttonadd
+        ' 
+        Buttonadd.Font = New Font("Microsoft Sans Serif", 8.25F)
+        Buttonadd.Location = New Point(524, 328)
+        Buttonadd.Name = "Buttonadd"
+        Buttonadd.Size = New Size(43, 48)
+        Buttonadd.TabIndex = 21
+        Buttonadd.Text = "+"
+        Buttonadd.UseVisualStyleBackColor = True
+        ' 
+        ' Button_4
+        ' 
+        Button_4.Font = New Font("Microsoft Sans Serif", 8.25F)
+        Button_4.Location = New Point(83, 195)
+        Button_4.Name = "Button_4"
+        Button_4.Size = New Size(43, 48)
+        Button_4.TabIndex = 22
+        Button_4.Text = "4"
+        Button_4.UseVisualStyleBackColor = True
+        ' 
+        ' Button_9
+        ' 
+        Button_9.Font = New Font("Microsoft Sans Serif", 8.25F)
+        Button_9.Location = New Point(274, 128)
+        Button_9.Name = "Button_9"
+        Button_9.Size = New Size(43, 48)
+        Button_9.TabIndex = 23
+        Button_9.Text = "9"
+        Button_9.UseVisualStyleBackColor = True
+        ' 
+        ' Button_8
+        ' 
+        Button_8.Font = New Font("Microsoft Sans Serif", 8.25F)
+        Button_8.Location = New Point(176, 128)
+        Button_8.Name = "Button_8"
+        Button_8.Size = New Size(43, 48)
+        Button_8.TabIndex = 24
+        Button_8.Text = "8"
+        Button_8.UseVisualStyleBackColor = True
+        ' 
+        ' Button_1
+        ' 
+        Button_1.Font = New Font("Microsoft Sans Serif", 8.25F)
+        Button_1.Location = New Point(83, 263)
+        Button_1.Name = "Button_1"
+        Button_1.Size = New Size(43, 48)
+        Button_1.TabIndex = 25
+        Button_1.Text = "1"
+        Button_1.UseVisualStyleBackColor = True
+        ' 
+        ' Button_6
+        ' 
+        Button_6.Font = New Font("Microsoft Sans Serif", 8.25F)
+        Button_6.Location = New Point(274, 195)
+        Button_6.Name = "Button_6"
+        Button_6.Size = New Size(43, 48)
+        Button_6.TabIndex = 26
+        Button_6.Text = "6"
+        Button_6.UseVisualStyleBackColor = True
+        ' 
+        ' Button_5
+        ' 
+        Button_5.Font = New Font("Microsoft Sans Serif", 8.25F)
+        Button_5.Location = New Point(176, 195)
+        Button_5.Name = "Button_5"
+        Button_5.Size = New Size(43, 48)
+        Button_5.TabIndex = 27
+        Button_5.Text = "5"
+        Button_5.UseVisualStyleBackColor = True
+        ' 
+        ' ButtonEqual
+        ' 
+        ButtonEqual.Font = New Font("Microsoft Sans Serif", 8.25F)
+        ButtonEqual.Location = New Point(274, 328)
+        ButtonEqual.Name = "ButtonEqual"
+        ButtonEqual.Size = New Size(43, 48)
+        ButtonEqual.TabIndex = 28
+        ButtonEqual.Text = "="
+        ButtonEqual.UseVisualStyleBackColor = True
+        ' 
+        ' Button_3
+        ' 
+        Button_3.Font = New Font("Microsoft Sans Serif", 8.25F)
+        Button_3.Location = New Point(274, 263)
+        Button_3.Name = "Button_3"
+        Button_3.Size = New Size(43, 48)
+        Button_3.TabIndex = 29
+        Button_3.Text = "3"
+        Button_3.UseVisualStyleBackColor = True
+        ' 
+        ' Button_2
+        ' 
+        Button_2.Font = New Font("Microsoft Sans Serif", 8.25F)
+        Button_2.Location = New Point(176, 263)
+        Button_2.Name = "Button_2"
+        Button_2.Size = New Size(43, 48)
+        Button_2.TabIndex = 30
+        Button_2.Text = "2"
+        Button_2.UseVisualStyleBackColor = True
+        ' 
+        ' Button_0
+        ' 
+        Button_0.Font = New Font("Microsoft Sans Serif", 8.25F)
+        Button_0.Location = New Point(83, 328)
+        Button_0.Name = "Button_0"
+        Button_0.Size = New Size(43, 48)
+        Button_0.TabIndex = 31
+        Button_0.Text = "0"
+        Button_0.UseVisualStyleBackColor = True
+        ' 
+        ' Buttondot
+        ' 
+        Buttondot.Font = New Font("Microsoft Sans Serif", 8.25F)
+        Buttondot.Location = New Point(176, 328)
+        Buttondot.Name = "Buttondot"
+        Buttondot.Size = New Size(43, 48)
+        Buttondot.TabIndex = 32
+        Buttondot.Text = "."
+        Buttondot.UseVisualStyleBackColor = True
+        ' 
+        ' clear
+        ' 
+        clear.Font = New Font("Microsoft Sans Serif", 8.25F)
+        clear.Location = New Point(83, 390)
+        clear.Name = "clear"
+        clear.Size = New Size(43, 48)
+        clear.TabIndex = 33
+        clear.Text = "C"
+        clear.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(clear)
+        Controls.Add(Buttondot)
+        Controls.Add(Button_0)
+        Controls.Add(Button_2)
+        Controls.Add(Button_3)
+        Controls.Add(ButtonEqual)
+        Controls.Add(Button_5)
+        Controls.Add(Button_6)
+        Controls.Add(Button_1)
+        Controls.Add(Button_8)
+        Controls.Add(Button_9)
+        Controls.Add(Button_4)
+        Controls.Add(Buttonadd)
+        Controls.Add(Buttonmulti)
+        Controls.Add(Buttonsub)
+        Controls.Add(Button_7)
+        Controls.Add(Buttondiv)
+        Controls.Add(txtDisplay)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents txtDisplay As TextBox
+    Friend WithEvents Buttondiv As Button
+    Friend WithEvents Button_7 As Button
+    Friend WithEvents Buttonsub As Button
+    Friend WithEvents Buttonmulti As Button
+    Friend WithEvents Buttonadd As Button
+    Friend WithEvents Button_4 As Button
+    Friend WithEvents Button_9 As Button
+    Friend WithEvents Button_8 As Button
+    Friend WithEvents Button_1 As Button
+    Friend WithEvents Button_6 As Button
+    Friend WithEvents Button_5 As Button
+    Friend WithEvents ButtonEqual As Button
+    Friend WithEvents Button_3 As Button
+    Friend WithEvents Button_2 As Button
+    Friend WithEvents Button_0 As Button
+    Friend WithEvents Buttondot As Button
+    Friend WithEvents clear As Button
+
+End Class
---
+++ CalculatorVB/Form1.vb
@@ -0,0 +1,144 @@
+﻿Public Class Form1
+    Dim value As Double = 0
+    Dim operation As String = ""
+    Dim operation_pressed As Boolean = False
+    Private Sub txtDisplay_TextChanged(sender As Object, e As EventArgs) Handles txtDisplay.TextChanged
+
+    End Sub
+
+    Private Sub Button_7_Click(sender As Object, e As EventArgs) Handles Button_7.Click
+        If txtDisplay.Text = "0" Then
+            txtDisplay.Text = "7"
+        Else
+            txtDisplay.Text &= "7"
+        End If
+
+    End Sub
+
+    Private Sub Button_8_Click(sender As Object, e As EventArgs) Handles Button_8.Click
+        If txtDisplay.Text = "0" Then
+            txtDisplay.Text = "8"
+        Else
+            txtDisplay.Text &= "8"
+        End If
+
+    End Sub
+
+    Private Sub Button_9_Click(sender As Object, e As EventArgs) Handles Button_9.Click
+        If txtDisplay.Text = "0" Then
+            txtDisplay.Text = "9"
+        Else
+            txtDisplay.Text &= "9"
+        End If
+    End Sub
+
+    Private Sub Button_4_Click(sender As Object, e As EventArgs) Handles Button_4.Click
+        If txtDisplay.Text = "0" Then
+            txtDisplay.Text = "4"
+        Else
+            txtDisplay.Text &= "4"
+        End If
+    End Sub
+
+    Private Sub Button_5_Click(sender As Object, e As EventArgs) Handles Button_5.Click
+        If txtDisplay.Text = "0" Then
+            txtDisplay.Text = "5"
+        Else
+            txtDisplay.Text &= "5"
+        End If
+    End Sub
+
+    Private Sub Button_6_Click(sender As Object, e As EventArgs) Handles Button_6.Click
+        If txtDisplay.Text = "0" Then
+            txtDisplay.Text = "6"
+        Else
+            txtDisplay.Text &= "6"
+        End If
+    End Sub
+
+    Private Sub Button_1_Click(sender As Object, e As EventArgs) Handles Button_1.Click
+        If txtDisplay.Text = "0" Then
+            txtDisplay.Text = "1"
+        Else
+            txtDisplay.Text &= "1"
+        End If
+    End Sub
+
+    Private Sub Button_2_Click(sender As Object, e As EventArgs) Handles Button_2.Click
+        If txtDisplay.Text = "0" Then
+            txtDisplay.Text = "2"
+        Else
+            txtDisplay.Text &= "2"
+        End If
+    End Sub
+
+    Private Sub Button_3_Click(sender As Object, e As EventArgs) Handles Button_3.Click
+        If txtDisplay.Text = "0" Then
+            txtDisplay.Text = "3"
+        Else
+            txtDisplay.Text &= "3"
+        End If
+    End Sub
+
+    Private Sub Button_0_Click(sender As Object, e As EventArgs) Handles Button_0.Click
+        If txtDisplay.Text = "0" Then
+            txtDisplay.Text = "0"
+        Else
+            txtDisplay.Text &= "0"
+        End If
+    End Sub
+
+    Private Sub Buttonadd_Click(sender As Object, e As EventArgs) Handles Buttonadd.Click
+        operation = "+"
+        value = Val(txtDisplay.Text)
+        operation_pressed = True
+        txtDisplay.Text = "0"
+    End Sub
+
+    Private Sub Buttonsub_Click(sender As Object, e As EventArgs) Handles Buttonsub.Click
+        operation = "-"
+        value = Val(txtDisplay.Text)
+        operation_pressed = True
+        txtDisplay.Text = "0"
+    End Sub
+
+    Private Sub Buttonmulti_Click(sender As Object, e As EventArgs) Handles Buttonmulti.Click
+        operation = "*"
+        value = Val(txtDisplay.Text)
+        operation_pressed = True
+        txtDisplay.Text = "0"
+    End Sub
+
+    Private Sub Buttondiv_Click(sender As Object, e As EventArgs) Handles Buttondiv.Click
+        operation = "/"
+        value = Val(txtDisplay.Text)
+        operation_pressed = True
+        txtDisplay.Text = "0"
+    End Sub
+
+    Private Sub ButtonEqual_Click(sender As Object, e As EventArgs) Handles ButtonEqual.Click
+        Dim second As Double
+        second = Val(txtDisplay.Text)
+
+        If operation = "+" Then
+            txtDisplay.Text = value + second
+        ElseIf operation = "-" Then
+            txtDisplay.Text = value - second
+        ElseIf operation = "*" Then
+            txtDisplay.Text = value * second
+        ElseIf operation = "/" Then
+            txtDisplay.Text = value / second
+        End If
+    End Sub
+
+    Private Sub clear_Click(sender As Object, e As EventArgs) Handles clear.Click
+        txtDisplay.Text = "0"
+        value = 0
+    End Sub
+
+    Private Sub Buttondot_Click(sender As Object, e As EventArgs) Handles Buttondot.Click
+        If Not txtDisplay.Text.Contains(".") Then
+            txtDisplay.Text &= "."
+        End If
+    End Sub
+End Class
